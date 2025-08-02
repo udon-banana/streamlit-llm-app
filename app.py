fdb9f4f92cfec579a5e5d5b3b78e3a9cc0a323eb
Shinya Matsukawa <matsukawashinya1987@gmail.com>
2025-08-02 23:53:35 +0900

    fix: remove old langchain.chat_models import

--- app.py
+++ app.py
@@ -4,7 +4,6 @@
 import os
 from dotenv import load_dotenv
 import streamlit as st
-from langchain.chat_models import ChatOpenAI
 from langchain_openai import ChatOpenAI
 from langchain.schema import SystemMessage, HumanMessage
 
